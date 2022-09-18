9eac76211f498d49fd23e0c153cda68097a11f32
DenianHoerlle <deniancloss@gmail.com>
2022-09-18 19:04:55 -0300

    finished context usage

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,7 +1,12 @@
+import {useContext} from "react";
+import {AuthContext} from "../../context";
+
 export default function Init() {
+    const {user} = useContext(AuthContext);
+
     return (
         <section>
-            <h3>Página Início</h3>
+            <h3>{user}</h3>
             <div></div>
         </section>
     );
